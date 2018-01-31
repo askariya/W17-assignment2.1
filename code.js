5cdf280ab1b63dc8afbe6431d756cb7e46e3d825
askariya <arunaluk@yahoo.ca>
2018-01-31 12:53:19 -0700

    Defined nChars, working on nWords

--- code.js
+++ code.js
@@ -2,16 +2,27 @@
 // this is just a stub for a function you need to implement
 //
 function getStats(txt) {
+
+    let nChars = txt.length; // Calculate number of characters
+
+    let nWords = calculateWordCount(txt.replace("\n", " "));
+
     return {
-        nChars: 123,
-        nWords: 22,
-        nLines: 10,
-        nNonEmptyLines: 22,
-        averageWordLength: 3.3,
-        maxLineLength: 33,
-        palindromes: ["12321", "kayak", "mom"],
-        longestWords: ["xxxxxxxxx", "123444444"],
-        mostFrequentWords: ["hello(7)", "world(1)"]
+        nChars: nChars,
+        // nWords: 22,
+        // nLines: 10,
+        // nNonEmptyLines: 22,
+        // averageWordLength: 3.3,
+        // maxLineLength: 33,
+        // palindromes: ["12321", "kayak", "mom"],
+        // longestWords: ["xxxxxxxxx", "123444444"],
+        // mostFrequentWords: ["hello(7)", "world(1)"]
     };
 }
 
+
+function calculateWordCount(txt){
+    var count = 0;
+    string = string.replace(/[^a-zA-Z0-9]/g, ' '); //replaces all special characters
+    return count;
+}
