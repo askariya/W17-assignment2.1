45b81f32e58c314ee65453c3c503d17b1e2a9174
askariya <arunaluk@yahoo.ca>
2018-02-02 22:16:54 -0700

    Completing assignment

--- code.js
+++ code.js
@@ -1,38 +1,116 @@
 //
 // this is just a stub for a function you need to implement
-//
+//SHould review this: https://vincent.billey.me/pure-javascript-immutable-array/
+
 function getStats(txt) {
 
     let nChars = txt.length; // Calculate number of characters
-    let nWords = calculateWordCount(txt); // Calculate number of words
-
-    let line_values = calculateLines(txt); 
+    let word_values = calculateWords(txt); // Calculate values pertaining to words
+    let line_values = calculateLines(txt);  // Calculate values pertaining to lines
+    let word_freqs = calculateWordFreq(txt); // Calculate word frequency
 
 
     return {
         text: txt,
         nChars: nChars,
-        nWords: nWords,
+        nWords: word_values.count,
         nLines: line_values.count,
         nNonEmptyLines: line_values.ne_count,
-        // averageWordLength: 3.3,
+        averageWordLength: word_values.avg_length,
         maxLineLength: line_values.max_line_length,
-        // palindromes: ["12321", "kayak", "mom"],
-        // longestWords: ["xxxxxxxxx", "123444444"],
-        // mostFrequentWords: ["hello(7)", "world(1)"]
+        palindromes: word_values.palindromes,
+        longestWords: word_values.top_ten_words,
+        mostFrequentWords: word_freqs
     };
 }
 
+/*
+    Calculates Word Freq and returns result in an array
+*/
+function calculateWordFreq(txt){
+    word_freqs = []; // array of pairs to hold words and counts
 
-function calculateWordCount(txt){
-    word_list = txt.replace(/[^a-zA-Z0-9]/g, ' ').split(' ').filter(x => x); //replaces all special characters with spaces
-    count = word_list.length;
-    word_length_total = 0; 
-    for(i = 0; i < word_list.length; i++){
-        //TODO
+    let words = txt.replace(/[^a-zA-Z0-9]/g, ' ').split(' ').filter(x => x);
+    words = words.sort(function(a, b) {
+        return a.localeCompare(b); // sort by dictionary order
+    });
+
+    // converts every value in words to lowercase
+    words = words.map(word => word.toLowerCase()); 
+
+    let prev_word = null;
+    let count = 0;
+
+    // calculate word frequency
+    for(i = 0; i < words.length; i++){
+        if(prev_word == null){
+            prev_word = words[i];
+            count = 1;
+        }
+        else if(prev_word == words[i]){
+            count++;
+        }
+
+        else if(prev_word != words[i]){
+            word_freqs.push([prev_word, count]);
+            prev_word = words[i];
+            count = 1;
+        }
+    }
+    word_freqs.push([prev_word, count]);
+
+    word_freqs.sort(function(a, b) {
+        return b[1] - a[1] || // sort by count
+                a[0].localeCompare(b[0]); // then sort by alphabetical order
+    });
+
+    // reformat array to be strings in the stylized format
+    for (i = 0; i < word_freqs.length; i++){
+        word_freqs[i] = word_freqs[i][0] + "(" + word_freqs[i][1] + ")";
+    }
+
+    word_freqs = word_freqs.slice(0,10); // get top 10
+
+    return word_freqs;
+}
+
+function calculateWords(txt){
+    //replaces all special characters with spaces
+    let words = txt.replace(/[^a-zA-Z0-9]/g, ' ').split(' ').filter(x => x);
+    let word_length_total = 0; 
+    let palindromes = [];
+    let top_ten_lwords = [];
+
+    for(i = 0; i < words.length; i++){
+        // total up word length
+        word_length_total += words[i].length; 
+        //check if palindrome
+        if(words[i].length > 2){
+
+            reverse_word = words[i].split('').reverse().join('');
+            if (words[i] == reverse_word){
+                palindromes.push(words[i]);  
+            } 
+        }
     } 
+    //calculate average word length
+    let avg_word_length = word_length_total/words.length;
 
-    return count;
+    //find top 10 words
+    // sort function referred to from here: 
+    // https://stackoverflow.com/questions/10630766/sort-an-array-based-on-the-length-of-each-element
+    top_ten = words.sort(function(a, b) {
+        return b.length - a.length || // sort by length, if equal then
+               a.localeCompare(b);    // sort by alphabetical order
+    });
+    top_ten = top_ten.slice(0,10); // get top 10
+
+    return {
+        count: words.length,
+        avg_length: avg_word_length,
+        palindromes: palindromes,
+        top_ten_words: top_ten
+    };
 }
 
 //let result = (txt.match(/\n/g) || []).length + 1;
@@ -58,6 +136,4 @@ function calculateLines(txt){
         count: lines.length,
         max_line_length: max_line_length,
     };
-
-    // return ne_count;
 }
